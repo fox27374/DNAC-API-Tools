e288ce283ba430a5c08b16298960d34fdb0cbe07
fox27374 <fox27374@gmail.com>
2019-02-07 13:11:26 +0000

    Config template

--- dnac_config.py
+++ dnac_config.py
@@ -1,5 +1,5 @@
 import os
-DNAC=os.environ.get('DNAC','172.24.89.199')
+DNAC=os.environ.get('DNAC','xxx.xxx.xxx.xxx')
 DNAC_PORT=os.environ.get('DNAC_PORT',443)
-DNAC_USER=os.environ.get('DNAC_USER','dkofler')
-DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','iseHxVc#99w&')
+DNAC_USER=os.environ.get('DNAC_USER','xxxxxxx')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','xxxxxxx')
