5a7abc74e3690da598affd8b8a9af9c2d5c643bf
fox27374 <fox27374@gmail.com>
2019-02-07 14:09:22 +0000

    Changed gitignore

---
+++ dnac_config.py
@@ -0,0 +1,5 @@
+import os
+DNAC=os.environ.get('DNAC','xxx.xxx.xxx.xxx')
+DNAC_PORT=os.environ.get('DNAC_PORT',443)
+DNAC_USER=os.environ.get('DNAC_USER','xxxxxxx')
+DNAC_PASSWORD=os.environ.get('DNAC_PASSWORD','xxxxxx')
